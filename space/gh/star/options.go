b4037cc70019eb08762ad7150ccf84bc13f7f342
Milos Gajdos <milosgajdos83@gmail.com>
2021-02-10 21:53:24 +0000

    Refactor pace/gh/star package and update go modules

--- space/gh/star/options.go
+++ space/gh/star/options.go
@@ -1,30 +1,33 @@
 package star
 
-// Options provides gh options
+// Options provides GitHub scraper options.
 type Options struct {
-	User    string
-	Paging  int
+	// User s GitHub username
+	User string
+	// Paging size for GitHub API results
+	Paging int
+	// Workers for mapping repos
 	Workers int
 }
 
-// Option is gh option
+// Option is GitHub scraper option.
 type Option func(*Options)
 
-// Paging configures paging
+// Paging configures GitHub results paging
 func Paging(p int) Option {
 	return func(o *Options) {
 		o.Paging = p
 	}
 }
 
-// User configures gh user
+// User configures GitHub username.
 func User(u string) Option {
 	return func(o *Options) {
 		o.User = u
 	}
 }
 
-// Workers configures paging
+// Workers configures GitHub API results paging.
 func Workers(w int) Option {
 	return func(o *Options) {
 		o.Workers = w
