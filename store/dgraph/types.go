555d10280bd952035264663713b35fb03b0e2cf9
Milos Gajdos <milosgajdos83@gmail.com>
2021-02-13 19:52:27 +0000

    Add dgraph store types

---
+++ store/dgraph/types.go
@@ -0,0 +1,29 @@
+package dgraph
+
+import "time"
+
+type Resource struct {
+	Name       string            `json:"name,omitempty"`
+	Group      string            `json:"group,omitempty"`
+	Version    string            `json:"version,omitempty"`
+	Kind       string            `json:"kind,omitempty"`
+	Namespaced bool              `json:"namespaced,omitempty"`
+	Attrs      map[string]string `json:"attrs,omitempty"`
+	DType      []string          `json:"dgraph.type,omitempty"`
+}
+
+type Entity struct {
+	UID       string            `json:"uid,omitempty"`
+	XID       string            `json:"xid,omitempty"`
+	Name      string            `json:"name,omitempty"`
+	Namespace string            `json:"namespace,omitempty"`
+	Resource  Resource          `json:"resource,omitempty"`
+	Links     []Entity          `json:"links,omitempty"`
+	CreatedAt *time.Time        `json:"created_at,omitempty"`
+	Attrs     map[string]string `json:"attrs,omitempty"`
+	DType     []string          `json:"dgraph.type,omitempty"`
+
+	// Links facets
+	Relation string  `json:"links|relation,omitempty"`
+	Weight   float64 `json:"links|weight,omitempty"`
+}
